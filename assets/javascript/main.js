47f122a0cf5a593b9b18cd7f987b02252b286c57
thechutrain <chu.chuyalan@gmail.com>
2016-12-19 17:40:40 -0500

    most basic form that submits data to firebase

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,31 @@
-// alert("testing if refs properly");
+"use strict";
+// processForm
+function processForm(event){
+  // 0) prevent default of the page refreshing
+  event.preventDefault();
+  // 1) get all the form data
+  // const form = document.querySelector("form");
+  // const input_arr = Array.from(form.querySelectorAll("input"));
+  let firstName = $("#firstName").val().trim();
+  let lastName = $("#lastName").val().trim();
+  // console.log(firstName);
+  // console.log(lastName);
+
+  // 2) validate data
+  // TO DO
+
+  //3) upload the data to firebase!
+  let personRef = database.ref().push();
+  personRef.set({
+    firstName: firstName,
+    lastName: lastName,
+  });
+
+  // 4) clear form & alert user?
+  $("#firstName").val("");
+  $("#lastName").val("");
+
+}; // closes the processForm function
+
+// --------- Event Listeners ----------
+$("button[class$='submit']").on("click", processForm);
