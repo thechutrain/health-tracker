bc28076830b1108c218963dbba05c3af047da0eb
thechutrain <chu.chuyalan@gmail.com>
2016-12-19 18:11:59 -0500

    working form that submits some basic info

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,26 +4,29 @@ function processForm(event){
   // 0) prevent default of the page refreshing
   event.preventDefault();
   // 1) get all the form data
-  // const form = document.querySelector("form");
-  // const input_arr = Array.from(form.querySelectorAll("input"));
   let firstName = $("#firstName").val().trim();
   let lastName = $("#lastName").val().trim();
-  // console.log(firstName);
-  // console.log(lastName);
+  let gender = $("input[name=gender]:checked").val();
+  let age = $("#age").val().trim();
 
   // 2) validate data
-  // TO DO
+  // TO DO!!
 
   //3) upload the data to firebase!
   let personRef = database.ref().push();
   personRef.set({
     firstName: firstName,
     lastName: lastName,
+    gender: gender,
+    age: age,
+    timeStamp: moment().unix(), // save the time as a unix number
   });
 
   // 4) clear form & alert user?
   $("#firstName").val("");
   $("#lastName").val("");
+  $("input[name=gender]:checked").prop('checked', false);
+  $("#age").val("");
 
 }; // closes the processForm function
 
